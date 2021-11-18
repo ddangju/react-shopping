567b6cd55dc26c53a50bca2797455c59794187df
ddangju <duswn4338@gmail.com>
2021-11-18 22:58:16 +0900

    axios 통신으로 데이터 받아오기 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,25 @@ import React, { useState } from "react";
 import cardData from "./data.js";
 import BackImg from "./img/background.png";
 import Card from "./Card";
+import axios from "axios";
 
 function App() {
-  let [data] = useState(cardData);
+  let [data, SetData] = useState(cardData);
+  // let [data2, setData2] = useState([]);
+
+  let handleClick = () => {
+    <div>하이</div>;
+    axios
+      .get("https://codingapple1.github.io/shop/data2.json")
+      .then((result) => {
+        // setData2(result.data);
+        SetData([...data, ...result.data]);
+        // console.log(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <>
@@ -15,7 +31,13 @@ function App() {
         {data.map((item, idx) => {
           return <Card item={item} idx={idx} key={idx}></Card>;
         })}
+        {/* {data2.map((item, idx) => {
+          return <Card item={item} idx={idx} key={idx}></Card>;
+        })} */}
       </div>
+      <button className="btn btn-primary" onClick={handleClick}>
+        더보기
+      </button>
     </>
   );
 }
